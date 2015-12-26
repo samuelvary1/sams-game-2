81a42e4e0afc565b378d4aa00b676887f8e775c7
samuelvary1 <sam.vary@gmail.com>
2015-12-26 10:59:49 -0500

    dont touch environment anymore ya dummy

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,12 @@
-require "bundler/setup"
 
-require 'active_record'
+require 'bundler/setup'
 
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+$: << "."
+
+Dir['app/controllers/*.rb'].each {|f| require f}
+Dir['app/models/*.rb'].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
