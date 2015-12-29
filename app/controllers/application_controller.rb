b41b17d4dd91d7e9e019f517af0ebb73fbbfb820
samuelvary1 <sam.vary@gmail.com>
2015-12-29 12:51:49 -0500

    added barebones for each chapter, and hardcoded routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,39 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
+
+  # you will definitely want to change these into dynamic routes later on
+
+  get '/game/chapter1' do 
+  	erb :"chapters/chapter1"
+  end
+
+  get '/game/chapter2' do 
+  	erb :"chapters/chapter2"
+  end
+
+  get '/game/chapter3' do 
+  	erb :"chapters/chapter3"
+  end
+
+  get '/game/chapter4' do 
+  	erb :"chapters/chapter4"
+  end
+
+  get '/game/chapter5' do 
+  	erb :"chapters/chapter5"
+  end
+
+  get '/game/chapter6' do 
+  	erb :"chapters/chapter6"
+  end
+
+  get '/game/victory' do 
+  	erb :"chapters/victory"
+  end
+
+
+
+
+
 end
