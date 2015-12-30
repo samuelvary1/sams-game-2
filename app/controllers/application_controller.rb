b8e1669552a0e7202cd7031512d1b4909beb473a
samuelvary1 <sam.vary@gmail.com>
2015-12-30 12:32:40 -0500

    set up character selection to start game, and areas for each chapter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,24 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
+  get '/pickup' do 
+    erb :pickup
+  end
+
+  post '/pickup' do 
+    items = Item.all 
+    pickup = items.find do |item|
+      item.name == params[:item][:name]
+    end
+    if pickup.nil?
+      "Sorry, that item isn't here"
+    else
+
+      "You picked up #{pickup.name}"
+    end
+
+
+  end
 
   # you will definitely want to change these into dynamic routes later on
 
@@ -15,23 +33,43 @@ class ApplicationController < Sinatra::Base
   	erb :"chapters/chapter1"
   end
 
+  post '/game/chapter1' do 
+    @character = Character.find_by(first_name: params[:character][:first_name])
+
+    @character.save
+
+    @area1 = Location.all[0]
+
+    erb :"chapters/chapter1"
+  end
+
   get '/game/chapter2' do 
+    @area2 = Location.all[1]
+    
   	erb :"chapters/chapter2"
   end
 
   get '/game/chapter3' do 
+    @area3 = Location.all[2]
+
   	erb :"chapters/chapter3"
   end
 
   get '/game/chapter4' do 
+    @area4 = Location.all[3]
+
   	erb :"chapters/chapter4"
   end
 
   get '/game/chapter5' do 
+    @area5 = Location.all[4]
+
   	erb :"chapters/chapter5"
   end
 
   get '/game/chapter6' do 
+    @area6 = Location.all[5]
+
   	erb :"chapters/chapter6"
   end
 
--- app/models/character.rb
+++ app/models/character.rb
@@ -2,5 +2,11 @@ class Character < ActiveRecord::Base
 
 	has_many :items
 
+	def pickup_item(item, location)
+		self.items << item
+		location.items.delete(item)
+		item.location_id = nil
+		"You have picked up a #{item.name}."
+	end
 end
 
